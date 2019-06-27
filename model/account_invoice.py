ff6add8b0f6db69f24be4f1a29f956e2098cd91a
rquintanam <itgeopanama@gmail.com>
2019-06-27 13:10:24 -0500

    Update account_invoice.py

--- model/account_invoice.py
+++ model/account_invoice.py
@@ -69,7 +69,7 @@ class AccountInvoice(models.Model):
 				'ruc': self.partner_id.ruc,
 				'dv': self.partner_id.dv,
 				# 'sell_term': 'Contado' if self.type in ['out_invoice', 'in_invoice'] else 'Credito',
-				'sell_term': 'Contado' if self.payment_term_id in ['1'] else 'Credito',
+				'sell_term': 'Contado' if self.payment_term_id.id == 1 else 'Credito',
 				'total': self.amount_total,
 				'sub_total': self.amount_untaxed,
 				'transaction_type': 'invoice' if self.type in ['out_invoice', 'in_invoice'] else 'cnote',
